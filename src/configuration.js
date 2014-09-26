1b96318bcd3f44c576d125ec0d00f116057e7dd1
Jeff McCoy <code@jeffandshauna.net>
2014-09-26 00:05:36 -0400

    Added SOAP handlers

--- src/configuration.js
+++ src/configuration.js
@@ -13,7 +13,16 @@ function configuration(SP_CONFIG) {
 		'FIELD_JSON_TRAIL': '_JSON',
 
 		// For SP 2010 use 2.0 for 2013 it's 3.0.  This was added due to random 500 errors from a SP farm when this header wasn't sent (this is NOT required by the oData Spec)!
-		'ODATA_VERSION'   : '2.0'
+		'ODATA_VERSION'   : '2.0',
+
+		'SOAP': {
+
+			'userinfo': '<?xml version="1.0" encoding="utf-8"?><soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope"><soap12:Body><GetCurrentUserInfo xmlns="http://schemas.microsoft.com/sharepoint/soap/directory/" /></soap12:Body></soap12:Envelope>',
+
+			'groups': '<?xml version="1.0" encoding="utf-8"?><soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope"><soap12:Body><GetGroupCollectionFromUser xmlns="http://schemas.microsoft.com/sharepoint/soap/directory/"><userLoginName>_USER_</userLoginName></GetGroupCollectionFromUser></soap12:Body></soap12:Envelope>'
+
+		}
+
 
 	};
 
